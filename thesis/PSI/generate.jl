9e2388e7749ab2a2a46b221e9bac1bdcca88fd5e
wuxinyu <571440788@qq.com>
2024-01-17 11:25:28 +0800

    20240117

---
+++ thesis/PSI/generate.jl
@@ -0,0 +1,57 @@
+
+using ApproxOperator
+
+function generate2D(ni::Int,ns::Int)
+
+    etype = (:Quartic2D,:□,:CubicSpline,:Shape)
+    s = 4.1/(ni-1)
+    nm = 120
+
+    nodes = ApproxOperator.Node{(:𝐼,),1}[]
+    data = Dict([
+        :x=>(1,zeros(ni^2)),
+        :y=>(1,zeros(ni^2)),
+        :z=>(1,zeros(ni^2)),
+        :s₁=>(1,s*ones(ni^2)),
+        :s₂=>(1,s*ones(ni^2)),
+        :s₃=>(1,s*ones(ni^2)),
+    ])
+    for (j,y) in enumerate(0.0:1.0/(ni-1):1.0)
+        for (i,x) in enumerate(0.0:1.0/(ni-1):1.0)
+            node = ApproxOperator.Node{(:𝐼,),1}((ni*(j-1)+i,),data)
+            node.x = x
+            node.y = y
+            push!(nodes,node)
+        end
+    end
+
+    points = ApproxOperator.Node{(:𝑔,:𝐺,:𝐶,:𝑠),4}[]
+    data = Dict([
+        :x=>(2,zeros(ns^2)),
+        :y=>(2,zeros(ns^2)),
+        :z=>(2,zeros(ns^2)),
+        :𝝭=>(4,zeros(ni^2*ns^2)),
+        :∂𝝭∂x=>(4,zeros(ni^2*ns^2)),
+        :∂𝝭∂y=>(4,zeros(ni^2*ns^2)),
+        :∂²𝝭∂x²=>(4,zeros(ni^2*ns^2)),
+        :∂²𝝭∂y²=>(4,zeros(ni^2*ns^2)),
+        :∂²𝝭∂x∂y=>(4,zeros(ni^2*ns^2)),
+        :𝗠=>(0,zeros(nm)),
+        :∂𝗠∂x=>(0,zeros(nm)),
+        :∂𝗠∂y=>(0,zeros(nm)),
+        :∂²𝗠∂x²=>(0,zeros(nm)),
+        :∂²𝗠∂y²=>(0,zeros(nm)),
+        :∂²𝗠∂x∂y=>(0,zeros(nm)),
+    ])
+    for (j,y) in enumerate(0.0:1.0/(ns-1):1.0)
+        for (i,x) in enumerate(0.0:1.0/(ns-1):1.0)
+            n = ns*(j-1)+i
+            point = ApproxOperator.Node{(:𝑔,:𝐺,:𝐶,:𝑠),4}((0,n,1,ni^2*(n-1)),data)
+            point.x = x
+            point.y = y
+            push!(points,point)
+        end
+    end
+
+    return ApproxOperator.ReproducingKernel{etype...}((0,ni^2,nodes),(0,ns^2,points))
+end
---
+++ thesis/PSI/plot2d.jl
@@ -0,0 +1,80 @@
+
+include("generate.jl")
+
+using  CairoMakie
+
+ni = 20
+ns = 100
+a = generate2D(ni,ns)
+set∇𝝭!(a)
+set∇²𝝭!(a)
+
+I = 190
+p = collect(a.𝓒)[I]
+xi = p.x
+yi = p.y
+zi = -0.01
+xs = zeros(ns^2)
+ys = zeros(ns^2)
+𝝭 = zeros(ns^2)
+∂𝝭∂x = zeros(ns^2)
+∂𝝭∂y = zeros(ns^2)
+∂²𝝭∂x² = zeros(ns^2)
+∂²𝝭∂y² = zeros(ns^2)
+∂²𝝭∂x∂y = zeros(ns^2)
+𝓖 = a.𝓖
+for (i,ξ) in enumerate(𝓖)
+    N = ξ[:𝝭]
+    B₁ = ξ[:∂𝝭∂x]
+    B₂ = ξ[:∂𝝭∂y]
+    B₃ = ξ[:∂²𝝭∂x²]
+    B₄ = ξ[:∂²𝝭∂y²]
+    B₅ = ξ[:∂²𝝭∂x∂y]
+    xs[i] = ξ.x
+    ys[i] = ξ.y
+    𝝭[i] = N[I]
+    ∂𝝭∂x[i] = B₁[I]
+    ∂𝝭∂y[i] = B₂[I]
+    ∂²𝝭∂x²[i] = B₃[I]
+    ∂²𝝭∂y²[i] = B₄[I]
+    ∂²𝝭∂x∂y[i] = B₅[I]
+end
+
+f = Figure(fonts=(;regular="Arial"))
+ax = Axis3(
+    f[1,1],
+    xgridvisible=false,
+    ygridvisible=false,
+    zgridvisible=false,
+    xlabelvisible=false,
+    ylabelvisible=false,
+    zlabelvisible=false,
+    xticksvisible=false,
+    yticksvisible=false,
+    # zticksvisible=false,
+    xticklabelsvisible=false,
+    yticklabelsvisible=false,
+    # zticklabelsvisible=false,
+    zticklabelsize=40,
+)
+xlims!(ax, (0.0,1.0))
+ylims!(ax, (0.0,1.0))
+scatter!(xi, yi, zi, color=:black, markersize = 40, marker=:circle)
+
+# zlims!(ax, (-0.1,1.0))
+# surface!(xs, ys, 𝝭, colormap=:jet, alpha=0.6)
+# save("𝝭.png",f)
+
+zlims!(ax, (-10.0,10.0))
+surface!(xs, ys, ∂𝝭∂y, colormap=:jet, alpha=0.6)
+save("∂𝝭∂y.png",f)
+
+# zlims!(ax, (-40.0,40.0))
+# surface!(xs, ys, ∂²𝝭∂x∂y, colormap=:jet, alpha=0.6)
+# save("∂²𝝭∂x∂y.png",f)
+
+zlims!(ax, (-50,50.0))
+surface!(xs, ys, ∂²𝝭∂y², colormap=:jet, alpha=0.6)
+save("∂²𝝭∂y².png",f)
+
+f
